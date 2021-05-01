cc25c3f3e48196f5e597653f9c48db9faeaabd98
rnalin <nalin.renan@gmail.com>
2021-05-01 16:56:43 -0300

    Criada classe Categoria

--- src/main/java/com/renius/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/renius/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.renius.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.renius.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	@GetMapping
-	public String listar() {
-		return "REST funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listaCategoria = new ArrayList<>();
+		listaCategoria.add(cat1);
+		listaCategoria.add(cat2);
+		
+		return listaCategoria;
 	}
 	
 
